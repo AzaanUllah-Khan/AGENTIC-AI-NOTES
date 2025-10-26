078786334b6638d38e350770a252bbcf480676a0
AzaanUllah-Khan <azaanullahkhan291@gmail.com>
2025-10-26 15:44:09 +0500

    Updated the task based on my current knowledge

---
+++ Class 03/Task/extratask.py
@@ -0,0 +1,6 @@
+name = input("Enter your Name: ")
+age = input("Enter your age: ")
+course = input("Enter your Course Name: ")
+batch = input("Enter your batch number: ")
+
+print("My name is",name,"I am",age,"years old.","I am enrolled in",course,"Batch",batch)

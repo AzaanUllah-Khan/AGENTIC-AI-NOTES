171f5934290c4df1e6207cf0aa08bb67adcf677c
AzaanUllah-Khan <azaanullahkhan291@gmail.com>
2025-10-29 22:47:43 +0500

    Code updated

--- Class 04/Class Task/python.py
+++ Class 04/Class Task/python.py
@@ -2,3 +2,32 @@ name = "AzaanUllah Khan"
 age = str(16) #It is casted as a string so even if it is 16 it will be considered as a string
 print(type(age))
 print("My name is",name,"and I am",age,"years old")
+
+
+x,y,z = "Azaan","Ullah","Khan"
+print(x) #Azaan
+print(y) #Ullah
+print(z) #Khan
+
+x = y = z = "AzaanUllah Khan"
+print(x) #AzaanUllah Khan
+print(y) #AzaanUllah Khan
+print(z) #AzaanUllah Khan
+
+
+fruits = ["Banana","Apple","Cherry"]
+print("Fruits:",fruits)
+
+# Scope of variables
+
+scope = "Global"
+class function():
+    scope = "Local"
+    print(scope)
+print(scope)
+
+# List
+Students = ["Azaan","Ali","Ahmed"]
+
+#Tuple
+Teachers = ("Rizwan","Shahmeer","Iqbal")

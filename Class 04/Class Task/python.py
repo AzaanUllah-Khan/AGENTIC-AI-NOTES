36e870b9d890d01a7ec8c779b96b87db81a6b6ec
AzaanUllah-Khan <azaanullahkhan291@gmail.com>
2025-10-29 21:23:34 +0500

    casting and type implemented

--- Class 04/Class Task/python.py
+++ Class 04/Class Task/python.py
@@ -1,4 +1,4 @@
 name = "AzaanUllah Khan"
-age = 16
-
+age = str(16) #It is casted as a string so even if it is 16 it will be considered as a string
+print(type(age))
 print("My name is",name,"and I am",age,"years old")

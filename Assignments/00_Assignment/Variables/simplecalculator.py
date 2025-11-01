7c79f643f3dcab32546ed34d401f446d1cfe502f
AzaanUllah-Khan <azaanullahkhan291@gmail.com>
2025-11-01 06:58:40 +0500

    assignment1 completed

---
+++ Assignments/00_Assignment/Variables/simplecalculator.py
@@ -0,0 +1,9 @@
+# Objective: Practice numeric variables and arithmetic. Task: Create two variables num1 and num2 and print their sum, difference, product, and division.
+
+num1 = 36
+num2 = 10
+
+print("Sum:",num1+num2)
+print("Difference:",num1-num2)
+print("Product:",num1*num2)
+print("Division:",num1/num2)
